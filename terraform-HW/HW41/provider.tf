8f1a3d67847c03241a320af6c600f501bf24edc1
roman <k.ivanovconn@gmail.com>
2026-04-15 21:38:21 +0300

    tf fmt

--- terraform-HW/HW41/provider.tf
+++ terraform-HW/HW41/provider.tf
@@ -21,9 +21,9 @@ terraform {
 # -----------------------------------------------------------------------------
 provider "yandex" {
   # Аутентификация через переменные (значения в terraform.tfvars)
-#  token     = var.yc_token
-  cloud_id  = var.yc_cloud_id
-  folder_id = var.yc_folder_id
-  zone      = var.yc_zone
+  #  token     = var.yc_token
+  cloud_id                 = var.yc_cloud_id
+  folder_id                = var.yc_folder_id
+  zone                     = var.yc_zone
   service_account_key_file = var.sa_key_file # ✅ путь к key.json
 }
