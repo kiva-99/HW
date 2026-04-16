885584143a371ae72d0a9cf17a7b04df4d83d42f
roman <k.ivanovconn@gmail.com>
2026-04-16 15:02:57 +0300

    tf fmtv7

--- terraform-HW/HW41/backend.tf
+++ terraform-HW/HW41/backend.tf
@@ -1,19 +1,15 @@
 terraform {
   backend "s3" {
     endpoints = {
-      s3 = "https://storage.yandexcloud.net"
+      s3       = "https://storage.yandexcloud.net"
+      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g7n8p29nt9r3i5e3m5/etn3u760i32s83u0dn23"
     }
 
     bucket = "kko-hw41-tf-b85be757"
     key    = "hw41/terraform.tfstate"
     region = "ru-central1"
 
-    # 🔥 ВАЖНО: включаем YDB locking
-    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g7n8p29nt9r3i5e3m5/etn3u760i32s83u0dn23"
-    dynamodb_table    = "state-lock-table"
-
-    # ❌ УБРАЛИ старый механизм
-    # use_lockfile = true
+    dynamodb_table = "state-lock-table"
 
     skip_region_validation      = true
     skip_credentials_validation = true
